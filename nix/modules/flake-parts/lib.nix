aa6802cc939341e4ba27a7d76bed949cfefc581e
Cameron Smith <cameron.ray.smith@gmail.com>
2025-11-11 21:45:00 -0500

    fix(darwin): use darwinModules.common instead of nixosModules.common
    The mkMacosSystem function was importing nixosModules.common which has
    class "nixos". Recent Nix versions enforce strict module class checking,
    causing evaluation errors when nixos modules are imported into darwin
    configurations.
    
    This fix adds darwinModules.common with identical content and updates
    mkMacosSystem to use it, resolving the module class mismatch.

--- nix/modules/flake-parts/lib.nix
+++ nix/modules/flake-parts/lib.nix
@@ -1,4 +1,9 @@
-{ self, inputs, config, lib, ... }:
+{ self
+, inputs
+, config
+, lib
+, ...
+}:
 let
   specialArgsFor = rec {
     common = {
@@ -24,31 +29,34 @@ let
       ];
     };
 
-    # Common and useful setting across all platforms
-    common = { lib, ... }: {
-      nix = {
-        settings = {
-          # Use all CPU cores
-          max-jobs = lib.mkDefault "auto";
-          # Duh
-          experimental-features = lib.mkDefault "nix-command flakes";
+    common =
+      { lib, ... }:
+      {
+        nix = {
+          settings = {
+            max-jobs = lib.mkDefault "auto";
+            experimental-features = lib.mkDefault "nix-command flakes";
+          };
         };
       };
-    };
   };
 
   homeModules = {
-    common = { config, pkgs, ... }: {
-      # Sensible default for `home.homeDirectory`
-      home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/${config.home.username}";
+    common =
+      { config, pkgs, ... }:
+      {
+        # Sensible default for `home.homeDirectory`
+        home.homeDirectory = lib.mkDefault "/${
+          if pkgs.stdenv.isDarwin then "Users" else "home"
+        }/${config.home.username}";
 
-      # For macOS, $PATH must contain these.
-      home.sessionPath = lib.mkIf pkgs.stdenv.isDarwin [
-        "/etc/profiles/per-user/$USER/bin" # To access home-manager binaries
-        "/nix/var/nix/profiles/system/sw/bin" # To access nix-darwin binaries
-        "/usr/local/bin" # Some macOS GUI programs install here
-      ];
-    };
+        # For macOS, $PATH must contain these.
+        home.sessionPath = lib.mkIf pkgs.stdenv.isDarwin [
+          "/etc/profiles/per-user/$USER/bin" # To access home-manager binaries
+          "/nix/var/nix/profiles/system/sw/bin" # To access nix-darwin binaries
+          "/usr/local/bin" # Some macOS GUI programs install here
+        ];
+      };
   };
 
   darwinModules = {
@@ -64,6 +72,20 @@ let
         }
       ];
     };
+
+    # Common and useful setting across all platforms
+    common =
+      { lib, ... }:
+      {
+        nix = {
+          settings = {
+            # Use all CPU cores
+            max-jobs = lib.mkDefault "auto";
+            # Duh
+            experimental-features = lib.mkDefault "nix-command flakes";
+          };
+        };
+      };
   };
 in
 {
@@ -72,33 +94,47 @@ in
       nixos-unified.lib = {
         inherit specialArgsFor;
 
-        mkLinuxSystem = { home-manager ? false }: mod: inputs.nixpkgs.lib.nixosSystem {
-          # Arguments to pass to all modules.
-          specialArgs = specialArgsFor.nixos;
-          modules = [
-            ../configurations
-            nixosModules.common
-            mod
-          ] ++ lib.optional home-manager nixosModules.home-manager;
-        };
+        mkLinuxSystem =
+          { home-manager ? false
+          ,
+          }:
+          mod:
+          inputs.nixpkgs.lib.nixosSystem {
+            # Arguments to pass to all modules.
+            specialArgs = specialArgsFor.nixos;
+            modules = [
+              ../configurations
+              nixosModules.common
+              mod
+            ]
+            ++ lib.optional home-manager nixosModules.home-manager;
+          };
 
-        mkMacosSystem = { home-manager ? false }: mod: inputs.nix-darwin.lib.darwinSystem {
-          specialArgs = specialArgsFor.darwin;
-          modules = [
-            ../configurations
-            nixosModules.common
-            mod
-          ] ++ lib.optional home-manager darwinModules.home-manager;
-        };
+        mkMacosSystem =
+          { home-manager ? false
+          ,
+          }:
+          mod:
+          inputs.nix-darwin.lib.darwinSystem {
+            specialArgs = specialArgsFor.darwin;
+            modules = [
+              ../configurations
+              darwinModules.common
+              mod
+            ]
+            ++ lib.optional home-manager darwinModules.home-manager;
+          };
 
-        mkHomeConfiguration = pkgs: mod: inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs; # cf. https://github.com/nix-community/home-manager/issues/3075
-          extraSpecialArgs = specialArgsFor.common;
-          modules = [
-            homeModules.common
-            mod
-          ];
-        };
+        mkHomeConfiguration =
+          pkgs: mod:
+          inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs; # cf. https://github.com/nix-community/home-manager/issues/3075
+            extraSpecialArgs = specialArgsFor.common;
+            modules = [
+              homeModules.common
+              mod
+            ];
+          };
       };
     };
   };
